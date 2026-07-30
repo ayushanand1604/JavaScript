d450c3e0321637a63252e7b5d53858abb34a3d29
Ayush <activeu281@gmail.com>
2026-07-30 14:38:11 +0530

    using dom change html and css

--- basicsofDom/script.js
+++ basicsofDom/script.js
@@ -1,2 +1,7 @@
-let x=document.querySelector("h1");
-console.log(x);
+let x=document.querySelector("h2");
+x.style.color="red";
+x.style.backgroundColor="yellow";
+x.style.fontFamily="sans-serif";
+ let y=document.querySelector("h1");
+ y.style.backgroundColor="green";
+ y.innerHTML="Hello World";
